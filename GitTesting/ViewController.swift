7566dc558cd1537819d876eaffce45e66a68b41f
Akash <akash.smartsense@gmail.com>
2016-08-01 15:11:57 +0530

    initial push

---
+++ GitTesting/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitTesting
+//
+//  Created by smartSense on 01/08/16.
+//  Copyright © 2016 smartSense. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
